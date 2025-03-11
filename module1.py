20219193f51b1c5d97cbe2c0fe3ca5c20d664794
Laurent <lfaurie@free.fr>
2025-03-11 14:27:35 +0100

    phase1

--- module1.py
+++ module1.py
@@ -1,32 +1,75 @@
-import winsound
+from load import logger
+import os
 
-def beep():
+def workerCargo(in_s):
     global this
-    if (this.dobeep):
-        frequency = 1250  # Set Frequency in Hertz
-        duration = 150  # Set Duration in ms
-        winsound.Beep(frequency, duration)  
+    local_loop = 0
+    logger.info("workerCargo init")
+    Continue = True
+    Erreur = False
+    fname = os.path.join(this.LogDir,'Cargo.json')
+    table = []
+    while Continue :
+        logger.info(f'worker Cargo waiting')
+        if this.eventtfmCargo.wait():
+            eventr = this.event
+            this.event = ""
+            marketr = this.Market_ID
+            logger.info(f'workerCargo running receive {eventr} for Market {marketr}')             
+            if eventr == "Docked":
+                #if docked take marketid and cargo.json
+                try:
+                    filej = open(fname, 'r') 
+                    dockedCargo = json.load(filej)
+                    filej.close()
+                except:
+                    logger.info('worker Cargo Erreur loading Cargo.json')   
+            elif eventr == "Undocked":
+                #if undocked compare old json with new one
+                # si pas vide au depart
+                readok = True
+                try:
+                    cc = dockedCargo["Count"]
+                except:
+                    logger.info('cannot read dockedcargo from memory')
+                    readok = False
+                if readok:
+                    if (cc > 0):
+                        try:
+                            filej = open(fname, 'r') 
+                            undockedCargo = json.load(filej)
+                            filej.close()
+                        except:
+                            logger.info('worker Cargo Erreur loading Cargo.json')
+                        #logger.info(dockedCargo)
+                        #logger.info(undockedCargo)
+                        tete = {}
+                        tete['timestamp'] = undockedCargo['timestamp']
+                        tete['event'] = 'Deposit'
+                        tete['marketId'] = marketr
+                        table.append(tete)
+                        transactions = []
+                        transactions = get_diff(dockedCargo, undockedCargo)
+                        #logger.info(transactions)
+                        tete['commodities'] = transactions  
+                        jsonout = json.dumps(tete)
+                        #jsonoutstrip = jsonout.replace('"','')
+                        logger.info(jsonout)
+                        # send to SM
+                        erreur = SendToServer(jsonout)
+                        if (erreur):
+                            logger.info(f'cannot send Cargo to serveur')
+                    
+            elif eventr == "STOP":
+                Continue = False
+                break 
+            else:
+                logger.info('receive nawak')
+                logger.info(eventr)
+            this.eventtfmCargo.clear()     
+                                  
+    logger.info('fin workerCargo')
 
 
-def searchInLine(line):
-    #MyList = ["FSDJump", "Docked", "Undocked","ShipTargeted", "ShipyardSwap", "MarketSell", "MissionAccepted", "MissionCompleted", "MultiSellExplorationData", "SellExplorationData", "SearchAndRescue", "SellMicroResources", "SellOrganicData", "RedeemVoucher", "CommitCrime", "Died", "PVPKill", "Missions", "Location", "SquadronStartup" ]
-    MyList = [ "FSDJump", "ShipTargeted", "Docked", "Undocked", "Embark", "Disembark", "ShipyardSwap", "ShipLocker", "MarketSell", "MissionAccepted", "MissionCompleted", "MissionFailed", "MultiSellExplorationData", "RedeemVoucher", "SellExplorationData", "SearchAndRescue", "CommitCrime", "CarrierJumpCancelled", "CarrierJumpRequest", "CarrierStats", "Died", "PVPKill", "LauchDrone", "Materials", "Rank", "Progress", "Reputation", "EngineerProgress", "SquadronStartup", "LoadGame", "Location", "Powerplay", "Missions","StoredShips" ] 
-    #MyList = [ "fsdjump", "shiptargeted", "docked", "undocked", "embark", "disembark", "shipyardswap", "shiplocker", "marketsell", "missionaccepted", "missioncompleted", "missionfailed", "multisellexplorationdata", "redeemvoucher", "sellexplorationdata", "searchandrescue", "commitcrime", "carrierjumpcancelled", "carrierjumprequest", "carrierstats", "died", "pvpkill", "lauchdrone", "materials", "rank", "progress", "reputation", "engineerprogress", "squadronstartup", "loadgame", "location", "powerplay", "missions","storedships", "shutdown" ] 
-   
-   
-    indicedeb = line.find("event")
-    if (indicedeb == -1):
-        return None
-    indicedeb = indicedeb + 8
-    sub = line[indicedeb:]
-    indicefin = sub.find(",")
-    if (indicefin == -1):
-        return None
-    indicefin = indicefin -1
-    txt = sub[:indicefin]
-    for ev in MyList:
-        if (ev == txt):
-            #logger.info("Find " + txt)
-            return ev
-    #logger.info("NOT Find " + txt)    
-    return None
+
+
